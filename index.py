4099ab2e02e897c77daa9d6032dde77053b6c5f8
mrinconma <mrinconma02@uninpahu.edu.co>
2020-10-21 21:26:41 -0500

    Clase: condicionales

---
+++ index.py
@@ -0,0 +1,17 @@
+print("Ingresar búsqueda")
+description=input()#ingresa palabras clave
+#print(description+":)")
+print("buscar en el repositorio")
+if description=="Química":
+    print("Encontrar autor")
+    print("Ingrese autor:")
+    authorTest=input()
+    if authorTest=="Muñoz, Daniel":
+        print("Traiga autor")
+    else: 
+        print("Se va a hacer otra comparación")
+
+else:
+    print("Rehaga la búsqueda")
+
+
