b6848199ebe19a77c0df1fba08ceaf0d1285abac
mrinconma <mrinconma02@uninpahu.edu.co>
2020-10-21 21:54:36 -0500

    Clase: while

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 print("Ingresar búsqueda")
-description=input()#ingresa palabras clave
+"""description=input()#ingresa palabras clave
 #print(description+":)")
 print("buscar en el repositorio")
 if description=="Química":
@@ -12,6 +12,20 @@ if description=="Química":
         print("Se va a hacer otra comparación")
 
 else:
+    print("Rehaga la búsqueda")"""
+description=""
+while description!="Química":
+    description=input()
     print("Rehaga la búsqueda")
+
+print("buscar en el repositorio")
+print("Encontrar autor")
+print("Ingrese autor:")
+authorTest=input()
+if authorTest=="Muñoz, Daniel":
+    print("Traiga autor")
+else: 
+    print("Se va a hacer otra comparación")
+
 
 
