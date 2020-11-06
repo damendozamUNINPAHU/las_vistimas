e75b17de1f470725401744fa9b3236c66e5dbd2a
mrinconma <mrinconma02@uninpahu.edu.co>
2020-11-05 19:58:55 -0500

    código para generar la cita

---
+++ citaAPA1.py
@@ -0,0 +1,34 @@
+print("Ingrese los datos solicitados")
+condition = input(1)
+condition = input(2)
+condition = input(3)
+if condition:1
+else:
+    lastName = input("Ingrese los apellidos")
+    initialsName = input("Ingrese iniciales del nombre seguido de punto")
+    year = input("Ingrese año")
+    title = input("Ingrese título")
+    city = input("Ingrese ciudad")
+    publisher = input("Ingrese editorial")
+    link = input("Ingrese enlace")
+print(lastName + ", " + initialsName +" (" + year + "). " + title +  ". " + city + ". " + publisher + ". " + "Recuperado de: " + link)
+if condition:2
+else:
+    print("Ingrese los datos solicitados")
+    corporativeName = input("Ingrese Instituciòn")
+    year = input("Ingrese año")
+    title = input("Ingrese título")
+    city = input("Ingrese ciudad")
+    publisher = input("Ingrese editorial")
+    link = input("Ingrese enlace")
+    print(corporativeName +" (" + year + "). " + title +  ". " + city + ". " + publisher + ". " + "Recuperado de: " + link)
+if condition:3
+else:
+    print(3)
+    print("Ingrese los datos solicitados")
+    title = input("Ingrese título")
+    year = input("Ingrese año")
+    city = input("Ingrese ciudad")
+    publisher = input("Ingrese editorial")
+    link = input("Ingrese enlace")
+    print(title +" (" + year + "). " + city + ". " + publisher + ". " + "Recuperado de: " +  link)
