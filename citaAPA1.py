ee27021efd676e9ea5565e602fbddc73ea198376
mrinconma <mrinconma02@uninpahu.edu.co>
2020-11-23 19:51:27 -0500

    23/11/2020_Ajuste de Json en el código de la rama development

--- citaAPA1.py
+++ citaAPA1.py
@@ -1,3 +1,13 @@
+import json
+
+def inputData(type):
+    year = input("Ingrese año")
+    title = input("Ingrese título")
+    city = input("Ingrese ciudad")
+    publisher = input("Ingrese editorial")
+    link = input("Ingrese enlace")
+    returnJson='{​​"year":'+year+',"title":'+title+'"city":'+city+'"publisher":'+publisher+'"link":'+link+'}​​​'
+    
 condition = int(input("Ingrese los datos solicitados para generar la cita de un libro electrónico: 1. autor personal, 2. autor Institucional, 3. sin autor"))
 if condition==1:
     lastName = input("Ingrese los apellidos")
